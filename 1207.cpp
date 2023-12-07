d7f53d81a8d9c67d0e7f70232dd6f56e67cab01d
huHsinling <hu930802@gmail.com>
2023-12-08 00:03:49 +0800

    getstarted~

--- 1207.cpp
+++ 1207.cpp
@@ -4,15 +4,6 @@
 #include<ctime>
 #include<vector>
 using namespace std;
-//1207test
-//這是曾子恩打的
-//I'm a good boy
-//version2  
-//one
-//two
-=======
->>>>>>> a48d8d21ec79d210e8edc788cb4b760887d58882
-//branchtest!!!
 
 class Events{
     protected:

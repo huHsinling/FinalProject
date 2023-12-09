0b6fe3198a0c9f5cd1ef593e98862cc255f10be8
MilkTseng <ryan930703@gmail.com>
2023-12-09 14:59:02 +0800

    Game event

--- 1207.cpp
+++ 1207.cpp
@@ -11,11 +11,11 @@ class Events{
         const string eventDetail; //事件敘述，可以加一些故事情節之類的？
         const int scoreChange; //此事件會改變多少分數
         const int mood; //心情值
-        const int type;// type == 0 不做任何事 ； type == 1 or 2 yes or no ; type == 3 選擇課
+        const int type;// type == 0 不做任何事 ； type == 1 yes or no ; type == 2 選擇課
     public:
         Events(string eventName, string eventDetail, int scoreChange, int mood, int type): eventName(eventName), eventDetail(eventDetail), scoreChange(scoreChange), mood(mood), type(type)
         {}
-        int eventHappened() const ; //執行事件
+        int eventHappened(int& scoreChange, int& moodChange) const ; //執行事件，回傳 type
         string getEventName();
         string getEventDetail();
         ~Events(){};
@@ -27,9 +27,11 @@ string Events::getEventName(){
 string Events::getEventDetail(){
     return eventDetail;
 }
-int Events::eventHappened() const
+int Events::eventHappened(int& scoreChange, int& moodChange) const
 {
-    cout<<eventDetail<<endl;
+    cout << eventDetail << endl;
+    scoreChange = this->scoreChange;
+    moodChange = this->mood;
     return type;
 }
 
@@ -45,8 +47,11 @@ protected:
     static int courseCnt;//課程數量
 public:
     Course(int id, string n, int c, int sv, double ap, bool ir);
-    void print() const;
-    static void printTitle();
+    void printAll() const;//選課時印出所有課程資訊
+    void printIdName() const;//事件選擇時印出 id 及 name
+    void printIdNameCredit() const;//結算時印出 id, name, credit
+    static void printTitle();//選課時印出所有課程資訊的標題
+    static void printINCTitle();//結算時印出 id, name, credit 的標題
     void changeScore(int score);
     int getID()const{return id;};
     int getScore()const{return scoreVari;};
@@ -80,12 +85,8 @@ id(id), name(name), credit(credit), scoreVari(scoreVari), assignedProb(assignedP
     courseCnt++;
 }
 
-void Course::print() const{
-    cout << id << ". " << name;
-    int n = ((maxNameLen - name.length()) / 8) + 1;
-    for(int i = 0; i < n; i++)
-        cout << "\t";
-    cout << credit << "\t";
+void Course::printAll() const{
+    printIdNameCredit();
     if(scoreVari >= 0)
         cout << "+" << scoreVari;
     else
@@ -94,13 +95,30 @@ void Course::print() const{
     cout << endl;
 }
 
+void Course::printIdName() const{
+    cout << id << ". " << name << endl;
+}
+
+void Course::printIdNameCredit() const{
+    cout << id << ". " << name;
+    int n = ((maxNameLen - name.length()) / 8) + 1;
+    for(int i = 0; i < n; i++)
+        cout << "\t";
+    cout << credit << "\t";
+}
+
 void Course::printTitle(){
+    printINCTitle();
+    cout << "難度" << "\t" << "選上機率";
+    cout << endl;
+}
+
+void Course::printINCTitle(){
     cout << "編號 課程名稱";
     int n = ((maxNameLen - 8) / 8) + 1;
     for(int i = 0; i < n; i++)
         cout << "\t";
-    cout << "學分" << "\t" << "難度" << "\t" << "選上機率";
-    cout << endl;
+    cout << "學分" << "\t";
 }
 
 void Course::changeScore(int score){
@@ -149,6 +167,9 @@ class Player{
         void clearcourse();
         void move(int step);//丟骰後移動 step 格
         void moveToWeek(int week);//移動到第 week 格
+        int randomID() const;//回傳 courselist 中隨機一個課程的 id
+        void printCourse() const;//印出 courselist 所有課程的 id 及 name
+        bool idExist(int id) const;//coursrelist 中是否有此課程 id
 };
 Player::Player(string n):name(n){
     totalCredit = 0;
@@ -194,14 +215,17 @@ void Player::changeScore(int ID, int scorechange){
 }
 void Player::countpassfail(){
     int basicscore = getBasicscore();
+    Course::printINCTitle();
+    cout << "結果";
+
     for(int i = 0; i < courselist.size(); i++){
         int score = basicScore + courselist[i]->getScore();
-        cout << courselist[i]->getName() << " Credit: " << courselist[i]->getCredit() << " Score: " << score;
+        courselist[i]->printIdNameCredit();
         if(score < 60){
-            cout << " Fail" << endl;
+            cout << "Fail" << endl;
         }
         else{
-            cout << " Pass" << endl;
+            cout << "Pass" << endl;
             totalCredit += courselist[i]->getCredit();
             addPasscourse(*courselist[i]);
         }
@@ -216,6 +240,25 @@ void Player::moveToWeek(int week){
     this->week = week;
 }
 
+int Player::randomID() const{
+    int index = rand() % courselist.size();
+    int id = courselist[index]->getID();
+    return id;
+}
+
+void Player::printCourse() const{
+    for(int i = 0; i < courselist.size(); i++){
+        courselist[i]->printIdName();
+    }
+}
+
+bool Player::idExist(int id) const{
+    for(int i = 0; i < courselist.size(); i++){
+        if(id == courselist[i]->getID())
+            return true;
+    }
+    return false;
+}
 
 const int MAX_WEEK_NUM = 18;
 
@@ -226,6 +269,7 @@ private:
     const int totalSemester;//總共幾學期
     int semester;//目前是第幾學期
     vector<Events*> events;//列表所有事件
+    vector<Course*> courses;//列表所有課程
     Player player;//單人玩家
 public:
     Game(int totalSemester, int weekNum, string name);
@@ -275,6 +319,31 @@ void Game::printMap(){
 }
 
 void Game::event(){
+    int index = rand() % events.size();
+    int scoreChange = 0, moodChange = 0;
+    int type = events[index]->eventHappened(scoreChange, moodChange);
+    int id = 0;
+
+    //type = 0 or 1: 隨機選一堂課
+    if(type == 0 || 1){
+        id = player.randomID();
+    }
+
+    //type = 2: 請玩家選擇課程
+    else if(type == 2){
+        cout << "請選擇一堂課，輸入課程代號" << endl;
+        player.printCourse();
+        while(true){
+            cin >> id;
+            if(player.idExist(id))
+                break;
+            else
+                cout << "課程不存在，請再輸入一次" << endl;
+        }
+    }
+
+    player.changeScore(id, scoreChange);
+    player.changeMood(moodChange);
     
 }
 void Game::nextSemester(){
